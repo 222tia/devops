e6c047d2535d41abda54f28e49a193a09a540c42
Tia <112046117+222tia@users.noreply.github.com>
2024-11-11 01:55:39 +0000

    update tests for factorial

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ describe('Regular factorial value', function() {
 
 describe('First two terms', function() {
   it('should return 0 and 1 for first two terms', function() {
-      assert.strictEqual(main.fibonacci(0), 0);
-      assert.strictEqual(main.fibonacci(1), 1);
+      assert.strictEqual(main.factorial(0), 0);
+      assert.strictEqual(main.factorial(1), 1);
     });
 });
